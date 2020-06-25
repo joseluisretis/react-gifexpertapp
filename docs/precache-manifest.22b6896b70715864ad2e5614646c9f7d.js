40cf5c0bec1647710883a73a69ce7ab8d0a125b0
Jose Luis Retis <jlarm85@gmail.com>
2020-06-25 00:25:38 -0500

    docs agregado 2

---
+++ docs/precache-manifest.22b6896b70715864ad2e5614646c9f7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e282027eac8e54e49b729f5e3d68adf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dacd7dcae2e49e2af6be",
+    "url": "/static/css/main.2378ae76.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dacd7dcae2e49e2af6be",
+    "url": "/static/js/main.fb737dec.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
